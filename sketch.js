efc71ea47a5b50ad39f4043852a14b6af66ae484
Vaibhav Poddar <poddarvaibhav2013@gmail.com>
2020-02-26 22:58:26 +0530

    line sweep added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var WIDTH     = 600; 	// Width of canvas
 var HEIGHT    = 600; 	// Height of canvas
-var totalRect = 40; 	// Total number of rectangles wanted 
+var totalRect = 10; 	// Total number of rectangles wanted 
 var epsilon   = 5;		// minimum distance between any 2 rectangle
 var margin    = 20;		// outer Boundary Margin
 var minW      = 20; 	// minimum rectangle Width
@@ -9,7 +9,7 @@ var minH      = 20; 	// minimum rectangle Height
 var maxH      = 80; 	// maximum rectangle Height
 
 // -----------------------------------------------------------------
-
+// 					Layout design
 var xmin  = margin;                 // min rect.x1
 var xmax  = WIDTH  - margin; 		// max rect.x1	
 var ymin  = margin;             	// min rect.y1
@@ -23,6 +23,11 @@ var hr;
 var inputN;
 var inputText;
 var count = 0;
+// ----------------------------------------------------------------
+// 					Line Sweep
+
+var segments = [];
+var sweepLineStatus = [];
 
 function windowResized() {
 	centerCanvas();
@@ -37,18 +42,23 @@ function centerCanvas() {
 	newbtn.position(cnv.x + 10, cnv.y+HEIGHT+10);
 	corbtn.position(cnv.x + 10, cnv.y+HEIGHT+40);
 	inputText.position(cnv.x + 10, cnv.y+HEIGHT+70);
-	inputN.position(cnv.x + 250,    cnv.y+HEIGHT+70);
+	inputN.position(cnv.x + 250,   cnv.y+HEIGHT+70);
 }
 
 function resetRectangles(argument) {
-	count = 0;
+	count      = 0;
 	rectangles = [];
+	segments   = [];
+	sweepLineStatus = [];
+	console.clear();
 	background(0, 255, 0);
 	loop();
 }
 
 function setup() {
-	txt       = createDiv('<H2>Location of maximal empty rectangle!</H2>');
+	txt       = createDiv('<H2>Corner Stitching Sweep traversal!</H2>');
+	// txt       = createDiv('<H2>Location of maximal empty rectangle!</H2>');
+
   	cnv       = createCanvas(WIDTH, HEIGHT);
 	hr        = createP("<br>");
  	newbtn    = createButton('Reload Rectangles');
@@ -85,6 +95,13 @@ function draw() {
 	// for (var i = rectangles.length - 1; i >= 0; i--) {
 	// 	console.log(rectangles[i]);
 	// }
+	
+	getAllSegements();
+	sortSegments();
+	// removeDuplicates();
+	console.log("Removing Duplicates:", segments);
+	getdots();
+	console.log("Done");
 	noLoop();
 }
 
@@ -155,5 +172,189 @@ function getRandomRectangle(eps) {
 }
 
 function runCornerStitching(){
+	loop();
+}
 
+
+function showHorLine(){
+	console.log(rectangles);
+	rectangles.sort(function(r1, r2){
+		if(r1.x1 < r2.x1) return -1;
+		return 0;
+	});
+	console.log(rectangles);
+}
+
+
+class Segment{
+	constructor(x1, y1, x2, y2, R, opening){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.rect = R;
+		this.open = opening;
+	}
+}
+
+function getAllSegements() {
+	var x1, y1, x2, y2, S;
+	for (var i = rectangles.length - 1; i >= 0; i--) {
+		x1 = rectangles[i].x1;
+		y1 = rectangles[i].y1;
+		x2 = rectangles[i].x2;
+		y2 = rectangles[i].y2;
+		S  = new Segment(x1, y1, x2, y2, rectangles[i], true);
+		segments.push(S);
+		
+		x1 = rectangles[i].x4;
+		y1 = rectangles[i].y4;
+		x2 = rectangles[i].x3;
+		y2 = rectangles[i].y3;
+		S  = new Segment(x1, y1, x2, y2, rectangles[i], false);
+		segments.push(S);
+	}
+}
+
+function sortSegments() {
+	console.log("Before sorting:", segments);
+	segments.sort(function(s1, s2){
+		if(s1.y1 < s2.y1) return -1;
+		if(s1.y1 > s2.y1) return 1;
+		return 0;
+	});
+	console.log("After sorting:", segments);
+}
+
+function removeDuplicates(argument) {
+	var curr, next;
+	var result = [];
+	if(segments.length<1) return;
+	
+	result.push(segments[0]);
+	var curr;
+	for (var i = 1; i < segments.length; i++) {
+		curr = segments[i].y1;
+		last = result[result.length-1].y1;
+		if(curr==last){
+			continue;
+		}else{
+			result.push(segments[i]);
+		}
+	}
+	segments = [];
+	segments = result;
+	result = [];
+}
+
+
+function flooring(x) { // now in O(N) later have to do in O(logN)
+	var ans = -1;
+	for (var i = 0; i < sweepLineStatus.length; i++) {
+		if(sweepLineStatus[i].x1 < x)
+			ans = i;
+		else if(sweepLineStatus[i].x1 > x)
+			return ans;
+		else{ 		// used in deletion
+			ans = i;
+			return i;
+		}
+	}
+	return ans;
+}
+
+
+function getdots(){
+	var index;
+	var points = [];
+	var temp = [];
+	var left, right;
+	var isopen;
+
+	fill(0,0,255);
+	
+	for (var i = 0; i< segments.length; i++) {
+		console.log(temp);
+		index  = flooring(segments[i].x1);
+		isopen = segments[i].open;
+		if(isopen){
+			if(index == -1){
+				// no left exist
+				sweepLineStatus.splice(0, 0, segments[i]);
+				temp.splice(0, 0, segments[i].x1);
+				points.push({x: 0,     y:segments[i].y1});
+				
+				right = 1;
+				if(right == sweepLineStatus.length)
+					points.push({x: WIDTH, y:segments[i].y1});
+				else
+					points.push({x: sweepLineStatus[1].rect.x1, y:segments[i].y1});
+
+				// linedash(0, segments[i].y1, WIDTH, segments[i].y1);
+			}
+			else{
+				sweepLineStatus.splice(index+1, 0, segments[i]);
+				temp.splice(index+1, 0, segments[i].x1);
+				left  = index; 
+				points.push({x:sweepLineStatus[left].rect.x2, y:segments[i].y1});
+
+				right = index + 2;
+				if(right == sweepLineStatus.length)
+					points.push({x:WIDTH, y:segments[i].y1});
+				else
+					points.push({x:sweepLineStatus[right].rect.x1, y:segments[i].y1});
+			}
+
+			console.log(isopen, index, points[points.length-2], points[points.length-1], temp);
+
+		}else{
+			left  = index-1;
+			if(left == -1)
+				points.push({x:0, y:segments[i].y1});
+			else
+				points.push({x:sweepLineStatus[left].rect.x2, y:segments[i].y1});
+			
+			right = index+1; 
+			if(right == sweepLineStatus.length)
+				points.push({x:WIDTH, y:segments[i].y1});
+			else
+				points.push({x:sweepLineStatus[right].rect.x1, y:segments[i].y1});
+			
+			sweepLineStatus.splice(index, 1);
+			temp.splice(index, 0);
+			console.log(isopen, index, points[points.length-2], points[points.length-1], temp);
+		}
+
+		linedash(points[points.length-2].x, points[points.length-2].y, points[points.length-1].x, points[points.length-1].y);
+		console.log("h");
+		circle(points[points.length-1].x, points[points.length-1].y, 1); 
+		circle(points[points.length-2].x, points[points.length-2].y, 1); 
+	}
+
+	console.log(points);
+	fill(0,0,255);
+	for (var i = points.length - 1; i >= 0; i--) {
+		circle(points[i].x, points[i].y, 8); 
+	}
+}
+
+
+
+function linedash(x1, y1, x2, y2, delta=2, style = '-') {
+	// delta is both the length of a dash, the distance between 2 dots/dashes, and the diameter of a round
+	let distance = dist(x1,y1,x2,y2);
+	let dashNumber = distance/delta;
+	let xDelta = (x2-x1)/dashNumber;
+	let yDelta = (y2-y1)/dashNumber;
+
+	for (let i = 0; i < dashNumber; i+= 2) {
+		let xi1 = i*xDelta + x1;
+		let yi1 = i*yDelta + y1;
+		let xi2 = (i+1)*xDelta + x1;
+		let yi2 = (i+1)*yDelta + y1;
+
+		if (style == '-') { line(xi1, yi1, xi2, yi2); }
+		else if (style == '.') { point(xi1, yi1); }
+		else if (style == 'o') { ellipse(xi1, yi1, delta/2); }
+	}
 }
